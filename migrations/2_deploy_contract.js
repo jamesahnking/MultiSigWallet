e93b751a637e8ab4c3907596f3d4a0525ed6a890
James Ahn-King <jamesahnking@gmail.com>
2021-10-13 10:41:07 -0400

    completed wallet - no auto refresh - no disabled buttons  - no styling

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,9 +1,8 @@
-const { web3 } = require("@openzeppelin/test-helpers/src/setup");
-const Wallet = artifacts.require("MultiSigWallet");
+const MultiSigWallet = artifacts.require("MultiSigWallet");
 
-module.exports = async function (deployer, _network, accounts) {
-    await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
-    const wallet = await Wallet.deployed(); //pointer
-    await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000})// transfer tokens
+module.exports = async function(deployer, _network, accounts) {
+  await deployer.deploy(MultiSigWallet, [accounts[0], accounts[1], accounts[2]], 2);
+  const wallet = await MultiSigWallet.deployed();
+  await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 10000});
 };
-  
+
